4c1015346a9635de0433e7ef6750907539b22947
Marija <mcubic02@fesb.hr>
2026-06-15 14:57:56 +0200

    Changed hero section and colors

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,35 +1,71 @@
 import Button from './Button'
+import './Hero.css'
 
 const heroServices = [
-  { label: 'Boat rental', href: '#boat-tours' },
-  { label: 'Quad tours', href: '#quad-tours' },
-  { label: 'Safari tour', href: '#safari-tours' },
-  { label: 'Fishing charter', href: '#fishing-trips' },
+  { label: 'Boat Rental', href: '#boat-tours' },
+  { label: 'Quad Tours', href: '#quad-tours' },
+  { label: 'Fishing Trips', href: '#fishing-tours' },
+]
+
+const heroPhotosLeft = [
+  {
+    src: '/images/SpeedBoat.png',
+    alt: 'Speed boat on turquoise sea',
+    className: 'hero__photo hero__photo--tilt-left',
+  },
+  {
+    src: '/images/Quad.png',
+    alt: 'Quad tour on Hvar',
+    className: 'hero__photo hero__photo--tilt-right',
+  },
+  {
+    src: '/images/Kayak.avif',
+    alt: 'Kayak on the sea',
+    className: 'hero__photo hero__photo--tilt-left',
+  },
+]
+
+const heroPhotosRight = [
+  {
+    src: '/images/Fishing.png',
+    alt: 'Fishing trip catch',
+    className: 'hero__photo hero__photo--tilt-right',
+  },
+  {
+    src: '/images/Pasara.png',
+    alt: 'Pasara boat at sea',
+    className: 'hero__photo hero__photo--tilt-left',
+  },
+  {
+    src: '/images/SafariTour.png',
+    alt: 'Quad safari tour overlooking the sea',
+    className: 'hero__photo hero__photo--tilt-right',
+  },
 ]
 
 function Hero() {
   return (
     <header className="hero">
-      <video
-        className="hero__video"
-        autoPlay
-        muted
-        loop
-        playsInline
-        poster="https://images.unsplash.com/photo-1567899378494-47b05033f870?w=1920&q=80"
-      >
-        <source
-          src="/images/Aerial-Drone-Shot-Of-Marina-Bay-In-Adriatic-Sea-C-2025-12-17-11-32-06-Utc.mp4"
-          type="video/mp4"
-        />
-      </video>
+      <div className="hero__photos" aria-hidden="true">
+        <div className="hero__column hero__column--left">
+          {heroPhotosLeft.map((photo) => (
+            <figure key={photo.src} className={photo.className}>
+              <img src={photo.src} alt="" />
+            </figure>
+          ))}
+        </div>
 
-      <div className="hero__overlay" />
+        <div className="hero__column hero__column--right">
+          {heroPhotosRight.map((photo) => (
+            <figure key={photo.src} className={photo.className}>
+              <img src={photo.src} alt="" />
+            </figure>
+          ))}
+        </div>
+      </div>
 
       <div className="hero__content">
-        <h1 className="hero__title">
-          EXPLORE THE CROATIAN COAST YOUR WAY
-        </h1>
+        <h1 className="hero__title">EXPLORE THE CROATIAN COAST YOUR WAY</h1>
 
         <div className="hero__buttons">
           {heroServices.map((service) => (
